18140d4aa6a549781c87a50cae572eb27b9aa6c5
pelu97 <pedrolpandrade@gmail.com>
2022-06-12 10:47:17 -0300

    Modified Card component to accept a className prop

--- src/components/Ui/Card.tsx
+++ src/components/Ui/Card.tsx
@@ -4,13 +4,14 @@ import classes from "./Card.module.css";
 
 
 interface CardProps{
-    children: ReactNode
+    children: ReactNode,
+    className?: string | undefined
 }
 
 
 function Card(props: CardProps){
     return (
-        <div className={classes.card}>
+        <div className={`${classes.card} ${props.className}`}>
             {props.children}
         </div>
     );
